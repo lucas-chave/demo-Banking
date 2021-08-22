66996c77065c4ec9b473673ab3ca11e54d5bb3ba
lucas-chave <lcchavesr150@gmail.com>
2021-08-22 20:51:04 -0300

    correction Accounts component

--- demo-banking/src/components/Accounts.jsx
+++ demo-banking/src/components/Accounts.jsx
@@ -6,9 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Loading from './Loading';
 
 function Accounts() {
-
   const [data, setData] = useState([]);
-
   const result = async () => {
     try {
       const response = await fetchAPI();
