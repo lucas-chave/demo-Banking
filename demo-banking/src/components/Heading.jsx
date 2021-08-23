a61fb74564964120d72babad29ab35fc7740b979
lucas-chave <lcchavesr150@gmail.com>
2021-08-22 21:04:24 -0300

    corrections Heading component

--- demo-banking/src/components/Heading.jsx
+++ demo-banking/src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Heanding(props) {
+export function Heanding(props) {
   const { text } = props;
   return(
     <h2>{text}</h2>
