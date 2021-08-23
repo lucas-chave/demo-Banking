4df534540d277bd0fceaaf43b952d6b0aad6a511
lucas-chave <lcchavesr150@gmail.com>
2021-08-22 22:49:05 -0300

    function fetchAPI correnting

--- demo-banking/src/service/fetch.js
+++ demo-banking/src/service/fetch.js
@@ -1,4 +1,4 @@
 export async function fetchAPI()  {
-  const result = await ((await fetch('https://react-app-m2y.herokuapp.com/contacts')).json())
+  const result = await ((await fetch('https://react-app-m2y.herokuapp.com/contacts')).json());
   return result;
 }
